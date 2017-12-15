7ec9f2c4440795dc92f1de8a82ab33de24d90c5d
apratara <anggapratap@gmail.com>
2017-12-15 18:52:27 +0700

    debugged ShapiroRudinPark_TimeSync_BRAM M AXIS IP

--- HDL/ip_repo/ShapiroRudinPark_TimeSynchronizer_BRAM_1.0/hdl/ShapiroRudinPark_TimeSynchronizer_BRAM_v1_0_M00_AXIS.v
+++ HDL/ip_repo/ShapiroRudinPark_TimeSynchronizer_BRAM_1.0/hdl/ShapiroRudinPark_TimeSynchronizer_BRAM_v1_0_M00_AXIS.v
@@ -212,7 +212,7 @@
 	            tx_done <= 1'b0;                                                     
 	          end                                                                    
 	      end                                                                        
-	    else if (read_pointer == NUMBER_OF_OUTPUT_WORDS)                             
+	    else if (read_pointer == NUMBER_OF_OUTPUT_WORDS+2)                             
 	      begin                                                                      
 	        // tx_done is asserted when NUMBER_OF_OUTPUT_WORDS numbers of streaming data
 	        // has been out.                                                         
